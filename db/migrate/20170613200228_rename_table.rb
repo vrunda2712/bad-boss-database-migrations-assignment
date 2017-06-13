52313b96b3b6402556b53fd886ffadf1530ff295
vrunda2712 <angy.v27@gmail.com>
2017-06-13 16:04:54 -0400

    Add new migration for rename party_guest table

---
+++ db/migrate/20170613200228_rename_table.rb
@@ -0,0 +1,5 @@
+class RenameTable < ActiveRecord::Migration[5.0]
+  def change
+    rename_table :party_guests, :widgets
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170613195301) do
+ActiveRecord::Schema.define(version: 20170613200228) do
 
   create_table "locations", force: :cascade do |t|
     t.string "name"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20170613195301) do
     t.integer "quantity"
   end
 
-  create_table "party_guests", force: :cascade do |t|
+  create_table "widgets", force: :cascade do |t|
     t.string  "first_name"
     t.string  "last_name"
     t.text    "dietary_restrictions"
