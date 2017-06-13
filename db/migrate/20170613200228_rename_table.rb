3e4f274fccce54ed3c0f6639a8add2fc390f7643
vrunda2712 <angy.v27@gmail.com>
2017-06-13 16:36:58 -0400

    Add new migration to rename party_guests table to widgets

--- db/migrate/20170613200228_rename_table.rb
+++ db/migrate/20170613200228_rename_table.rb
@@ -1,5 +1,8 @@
 class RenameTable < ActiveRecord::Migration[5.0]
-  def change
+  def up
     rename_table :party_guests, :widgets
+  end
+  def down
+    rename_table :widgets, :party_guests
   end
 end
