8f1ff08acd4271cae6cb88bfc83c77fb81c09df7
vrunda2712 <angy.v27@gmail.com>
2017-06-13 16:35:49 -0400

    Add new migration to change datatype of quantity

--- db/migrate/20170613201502_rename_quantity_datatype.rb
+++ db/migrate/20170613201502_rename_quantity_datatype.rb
@@ -1,7 +1,12 @@
 class RenameQuantityDatatype < ActiveRecord::Migration[5.0]
-  def change
+  def up
     change_table :parts do |t|
       t.change :quantity, :decimal
+    end
+  end
+  def down
+    change_table :parts do |t|
+      t.change :quantity, :integer
     end
   end
 end
